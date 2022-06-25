9193ecb1c26ecac5c9bcf86fb9946a7fb89efa3e
syaiful505 <syaifulstyle505@gmail.com>
2022-06-25 22:24:39 +0700

    Slicing Navbar

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -5,11 +5,6 @@ const Home = () => {
 	return(
 		<div className="home">
 			<Navbar />
-			<div className="row">
-				<div className="col-2">
-					<h3>Hello World</h3>
-				</div>
-			</div>
 		</div>
 	)
 }
--- client/src/components/home/Navbar.jsx
+++ client/src/components/home/Navbar.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { FaFacebookSquare,FaYoutube,FaGithubSquare } from "react-icons/fa";
+import { ImLinkedin } from "react-icons/im";
+import { BsListUl } from "react-icons/bs";
 
-const Navbar = () => {
+export default function Navbar() {
 	return (
-		<h2>Navbar Page</h2>
+		<div id='navbar' className="navbar">
+			<div className="container">
+				<div className="row">
+					<input type="checkbox" id='toggle'/>
+					<div className="col-4">
+						<div className="image-menubar">
+							<Link className="image" to="/">
+								<h1 className="brand">&lt;/&gt; syaifulstyle</h1>
+							</Link>
+							<label className="menu_icon" htmlFor="toggle"><BsListUl /></label>
+						</div>
+					</div>
+					<div className="col-8">
+						<ul className="link-list toggle">
+							<li className="link-item">
+								<Link to="/about" className="navbar-item">About</Link>
+							</li>
+							<li className="link-item">
+								<Link to="/repository" className="navbar-item">Repository</Link>
+							</li>
+							<li className="link-item">
+								<Link to="/contact" className="navbar-item">Contact</Link>
+							</li>
+							<div className="social-icon">
+								<li className="link-item">
+									<Link to="/about"><span><FaGithubSquare/></span></Link>
+								</li>
+								<li className="link-item">
+									<Link to="/about"><span><ImLinkedin/></span></Link>
+								</li>
+								<li className="link-item">
+									<Link to="/about"><span><FaYoutube/></span></Link>
+								</li>
+								<li className="link-item">
+									<Link to="/about"><span><FaFacebookSquare/></span></Link>
+								</li>
+							</div>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
 	)
 };
-
-export default Navbar;
